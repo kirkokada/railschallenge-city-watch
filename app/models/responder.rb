1eaead9beefa91dea1db81f994a10e7c8447b2c8
Kirk Okada <kirkokada@gmail.com>
2015-04-20 23:37:13 -1000

    refactor

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -5,9 +5,9 @@ class Responder < ActiveRecord::Base
 
   validates :capacity, presence: true,
                        inclusion: { in: 1..5 }
-  validates :name, presence: true,
-                   uniqueness: { case_sensitive: false }
-  validates :type, presence: true
+  validates :name,     presence: true,
+                       uniqueness: { case_sensitive: false }
+  validates :type,     presence: true
 
   scope :of_type,     -> (type)     { where(type: type) }
   scope :unassigned,  ->            { where(emergency_code: nil) }
@@ -16,27 +16,6 @@ class Responder < ActiveRecord::Base
   scope :appropriate, -> (severity) { order("abs(capacity - #{severity}) asc") }
   scope :capable_of,  -> (severity) { where('capacity >= ?', severity) }
 
-  # Class methods
-
-  def self.allocate_responders(type, severity, code)
-    responders = of_type(type).available
-    if responders.capable_of(severity).any?
-      assign_responders(responders.capable_of(severity), severity, code)
-    else
-      assign_responders(responders, severity, code)
-    end
-  end
-
-  def self.assign_responders(responders, severity, code)
-    while severity > 0
-      return false if responders.empty?
-      responder = responders.appropriate(severity).first
-      responder.assign_code(code)
-      severity -= responder.capacity
-    end
-    true
-  end
-
   def self.capacities_for(type)
     capacities = []
     responders = of_type(type)
@@ -44,10 +23,6 @@ class Responder < ActiveRecord::Base
     capacities << capacity_of(responders.unassigned)
     capacities << capacity_of(responders.on_duty)
     capacities << capacity_of(responders.available)
-  end
-
-  def self.capacity_of(responders)
-    responders.sum(:capacity)
   end
 
   def self.dispatch_to(emergency)
@@ -67,17 +42,48 @@ class Responder < ActiveRecord::Base
     capacities
   end
 
-  # Instance Methods
-
-  def assign_code(code)
-    update_attribute(:emergency_code, code)
-  end
-
   def dismiss
     update_attribute(:emergency_code, nil)
   end
 
   def to_params
     name
+  end
+
+  def assign_code(code)
+    update_attribute(:emergency_code, code)
+  end
+
+  private
+
+  def self.allocate_responders(type, severity, code)
+    best_responder = most_appropriate_for(type, severity)
+    unless best_responder.nil?
+      best_responder.assign_code(code)
+      return true
+    else
+      assign_responders(type, severity, code)
+    end
+  end
+
+  def self.assign_responders(type, severity, code)
+    responders = of_type(type).available
+    while severity > 0
+      return false unless responders.any?
+      responder = responders.appropriate(severity).first
+      responder.assign_code(code)
+      severity -= responder.capacity
+    end
+
+    true
+  end
+
+  def self.most_appropriate_for(type, severity)
+    return nil if severity == 0
+    of_type(type).available.capable_of(severity).appropriate(severity).first
+  end
+
+  def self.capacity_of(responders)
+    responders.sum(:capacity)
   end
 end
