da9907e3de54936ec8b6eb562d03b398b2928f36
Kirk Okada <kirkokada@gmail.com>
2015-04-23 10:59:27 -1000

    Refactor Emergency validations

--- app/models/emergency.rb
+++ app/models/emergency.rb
@@ -1,12 +1,9 @@
 class Emergency < ActiveRecord::Base
   validates :code, presence: true,
                    uniqueness: { case_sensitive: false }
-  validates :police_severity,  presence: true,
-                               numericality: { greater_than_or_equal_to: 0 }
-  validates :fire_severity,    presence: true,
-                               numericality: { greater_than_or_equal_to: 0 }
-  validates :medical_severity, presence: true,
-                               numericality: { greater_than_or_equal_to: 0 }
+  validates :police_severity, :fire_severity, :medical_severity,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
 
   has_many :responders, foreign_key: :emergency_code, primary_key: :code
 
