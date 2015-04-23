d73222b81dfc292340657611265293324103edb5
Kirk Okada <kirkokada@gmail.com>
2015-04-23 09:19:13 -1000

    Add db level null validation for emergency severities

--- db/migrate/20150417232024_create_emergencies.rb
+++ db/migrate/20150417232024_create_emergencies.rb
@@ -2,9 +2,9 @@ class CreateEmergencies < ActiveRecord::Migration
   def change
     create_table :emergencies do |t|
       t.string :code,              null: false
-      t.integer :fire_severity,    default: 0
-      t.integer :police_severity,  default: 0
-      t.integer :medical_severity, default: 0
+      t.integer :fire_severity,    null: false
+      t.integer :police_severity,  null: false
+      t.integer :medical_severity, null: false
       t.boolean :full_response,    default: false
       t.time :resolved_at
 
